75b3a84881f9588a2530bd9b3df7f6e32389faf7
JonathanWibowo <wibowo.jonathan@ymail.com>
2017-05-02 19:15:54 +0700

    Update light.py

--- light.py
+++ light.py
@@ -2,8 +2,8 @@
 #link: https://thepihut.com/blogs/raspberry-pi-tutorials/27968772-turning-on-an-led-with-your-raspberry-pis-gpio-pins
 
 
-LIGHT ON & OFF
---------------------------
+#LIGHT ON & OFF
+#--------------------------
 import RPi.GPIO as GPIO
 import time
 
